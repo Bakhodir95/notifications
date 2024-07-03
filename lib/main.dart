ca1c9954eaed0195f23cce66a927bdd9292110e1
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-07-03 13:07:27 +0500

    last

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,69 @@ import 'package:notifications/services/local_notifications.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await LocalNotifications.requestPermission();
-  runApp(MyNotification());
+  // await LocalNotificationsService.start();
+  runApp(const MyApp());
 }
 
-class MyNotification extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (!LocalNotificationsService.notificationsEnabled)
+                const Text(
+                  "Siz xabarnomaga ruxsat bermadingiz shu sabab sizga xabarnomalar kelmaydi."
+                  "\nBuni to'g'irlash uchun sozlamalarga borib to'girlang",
+                ),
+              TextButton.icon(
+                  onPressed: () {
+                    // LocalNotifications.showNotification();
+                  },
+                  icon: Icon(Icons.message),
+                  label: Text("Oddiy Xabarnoma")),
+              TextButton.icon(
+                  onPressed: () {
+                    // LocalNotificationsService.showScheduledNotification();
+                  },
+                  icon: Icon(Icons.message),
+                  label: Text("Rejali Xabarnoma")),
+              TextButton.icon(
+                  onPressed: () {
+                    // LocalNotificationsService.showPeriodicNotification();
+                  },
+                  icon: Icon(Icons.message),
+                  label: Text("Davomiy Xabarnoma")),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/services/local_notifications.dart
+++ lib/services/local_notifications.dart
@@ -1,14 +1,22 @@
 import 'dart:io';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class LocalNotifications {
-  static final _locaNotification = FlutterLocalNotificationsPlugin();
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:timezone/data/latest_all.dart' as tzl;
+import 'package:timezone/timezone.dart' as tz;
+
+class LocalNotificationsService {
+  static final _localNotification = FlutterLocalNotificationsPlugin();
 
   static bool notificationsEnabled = false;
 
   static Future<void> requestPermission() async {
-    if (Platform.isAndroid || Platform.isIOS) {
-      notificationsEnabled = await _locaNotification
+    // Birinchi dasturimiz qaysi qurilmada run bo'layotganini tekshiramiz
+    if (Platform.isIOS || Platform.isMacOS) {
+      // Agar IOS bo'lsa unda
+      // shu kod orqali notification'ga ruxsat so'raymiz
+      notificationsEnabled = await _localNotification
               .resolvePlatformSpecificImplementation<
                   IOSFlutterLocalNotificationsPlugin>()
               ?.requestPermissions(
@@ -18,34 +26,184 @@ class LocalNotifications {
               ) ??
           false;
 
-      await _locaNotification
-              .resolvePlatformSpecificImplementation<
-                  MacOSFlutterLocalNotificationsPlugin>()
-              ?.requestPermissions(
-                alert: true,
-                badge: true,
-                sound: true,
-              ) ??
-          false;
+      // Agar MacOS bo'lsa unda
+      // bu kod orqali notification'ga ruxsat so'raymiz
+      await _localNotification
+          .resolvePlatformSpecificImplementation<
+              MacOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            alert: true,
+            badge: true,
+            sound: true,
+          );
     } else if (Platform.isAndroid) {
+      // Agar Android qurilma bo'lsa unda
+      // bu kod orqali android notification sozlamasini olamiz
       final androidImplementation =
-          await _locaNotification.resolvePlatformSpecificImplementation<
+          _localNotification.resolvePlatformSpecificImplementation<
               AndroidFlutterLocalNotificationsPlugin>();
+
+      // va bu yerda darhol xabarnomasiga ruxsat so'raymiz
       final bool? grantedNotificationPermission =
           await androidImplementation?.requestNotificationsPermission();
-      final bool? grantedScheduleNotificationPermission =
-          await androidImplementation?.requestExactAlarmsPermission();
 
+      // bu yerda esa rejali xabarnomaga ruxsat so'raymiz
+      final bool? grantedScheduledNotificationPermission =
+          await androidImplementation?.requestExactAlarmsPermission();
+      // bu yerda o'zgaruvchiga belgilab qo'yapmiz foydalanuvchi ruxsat berdimi yoki yo'q
       notificationsEnabled = grantedNotificationPermission ?? false;
-      notificationsEnabled = grantedScheduleNotificationPermission ?? false;
+      notificationsEnabled = grantedScheduledNotificationPermission ?? false;
     }
   }
 
   static Future<void> start() async {
-    // final currentTimeZone = await FlutterTimeline.get
+    // hozirgi joylashuv (timezone) bilan vaqtni oladi
+    final currentTimeZone = await FlutterTimezone.getLocalTimezone();
+    tzl.initializeTimeZones();
+    tz.setLocalLocation(tz.getLocation(currentTimeZone));
+
+    // android va IOS uchun sozlamalarni to'g'irlaymiz
+    const androidInit = AndroidInitializationSettings("notification_icon");
+    final iosInit = DarwinInitializationSettings(
+      notificationCategories: [
+        DarwinNotificationCategory(
+          'demoCategory',
+          actions: <DarwinNotificationAction>[
+            DarwinNotificationAction.plain('id_1', 'Action 1'),
+            DarwinNotificationAction.plain(
+              'id_2',
+              'Action 2',
+              options: <DarwinNotificationActionOption>{
+                DarwinNotificationActionOption.destructive,
+              },
+            ),
+            DarwinNotificationAction.plain(
+              'id_3',
+              'Action 3',
+              options: <DarwinNotificationActionOption>{
+                DarwinNotificationActionOption.foreground,
+              },
+            ),
+          ],
+          options: <DarwinNotificationCategoryOption>{
+            DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
+          },
+        )
+      ],
+    );
+
+    // umumiy sozlamalarga e'lon qilaman
+    final notificationInit = InitializationSettings(
+      android: androidInit,
+      iOS: iosInit,
+    );
+
+    // va FlutterLocalNotification klasiga sozlamalarni yuboraman
+    // u esa kerakli qurilma sozlamalarini to'g'irlaydi
+    await _localNotification.initialize(notificationInit);
   }
 
-  static Future<void> showNotifications() async {}
+  static void showNotification() async {
+    // android va ios uchun qanday
+    // turdagi xabarlarni ko'rsatish kerakligni aytamiz
+    const androidDetails = AndroidNotificationDetails(
+      "goodChannelId",
+      "goodChannelName",
+      importance: Importance.max,
+      priority: Priority.max,
+      playSound: true,
+      sound: RawResourceAndroidNotificationSound("notification"),
+      actions: [
+        AndroidNotificationAction('id_1', 'Action 1'),
+        AndroidNotificationAction('id_2', 'Action 2'),
+        AndroidNotificationAction('id_3', 'Action 3'),
+      ],
+    );
 
-  // static Future<void> requestPermission() async {}
+    const iosDetails = DarwinNotificationDetails(
+      sound: "notification.aiff",
+      categoryIdentifier: "demoCategory",
+    );
+
+    const notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iosDetails,
+    );
+
+    // show funksiyasi orqali darhol xabarnoma ko'rsatamiz
+    await _localNotification.show(
+      0,
+      "Birinchi NOTIFICATION",
+      "Salom sizga \$1,000,000 pul tushdi. SMS kodni ayting!",
+      notificationDetails,
+    );
+  }
+
+  static void showScheduledNotification() async {
+    // android va ios uchun qanday
+    // turdagi xabarlarni ko'rsatish kerakligni aytamiz
+    const androidDetails = AndroidNotificationDetails(
+      "goodChannelId",
+      "goodChannelName",
+      importance: Importance.max,
+      priority: Priority.max,
+      playSound: true,
+      sound: RawResourceAndroidNotificationSound("notification"),
+      ticker: "Ticker",
+    );
+
+    const iosDetails = DarwinNotificationDetails(
+      sound: "notification.aiff",
+    );
+
+    const notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iosDetails,
+    );
+
+    // show funksiyasi orqali darhol xabarnoma ko'rsatamiz
+    await _localNotification.zonedSchedule(
+      0,
+      "Birinchi NOTIFICATION",
+      "Salom sizga \$1,000,000 pul tushdi. SMS kodni ayting!",
+      tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      payload: "Salom",
+    );
+  }
+
+  static void showPeriodicNotification() async {
+    // android va ios uchun qanday
+    // turdagi xabarlarni ko'rsatish kerakligni aytamiz
+    const androidDetails = AndroidNotificationDetails(
+      "goodChannelId",
+      "goodChannelName",
+      importance: Importance.max,
+      priority: Priority.max,
+      playSound: true,
+      sound: RawResourceAndroidNotificationSound("notification"),
+      ticker: "Ticker",
+    );
+
+    const iosDetails = DarwinNotificationDetails(
+      sound: "notification.aiff",
+    );
+
+    const notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iosDetails,
+    );
+
+    // show funksiyasi orqali darhol xabarnoma ko'rsatamiz
+    await _localNotification.periodicallyShowWithDuration(
+      0,
+      "Birinchi NOTIFICATION",
+      "Salom sizga \$1,000,000 pul tushdi. SMS kodni ayting!",
+      const Duration(minutes: 1),
+      notificationDetails,
+      payload: "Salom",
+    );
+  }
 }
